87bec13911d94b41acb7889bb8889e4f83aa7805
Francis Rodrigues <francis.rod01@outlook.com>
2021-07-02 22:20:58 -0300

    test: beer, dto builder, controller, service and utils

---
+++ src/test/java/one/digitalinnovation/beerstock/BeerStockApplicationTests.java
@@ -0,0 +1,12 @@
+package one.digitalinnovation.beerstock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
